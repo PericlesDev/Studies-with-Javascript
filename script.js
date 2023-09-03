f2b05f897b45047c1c31ce9dabe8e693b332e121
unknown <PericlesDosSantosRamos@hotmail.com>
2023-09-02 21:40:00 -0300

    Desenvolvendo caixas de diálogo com comandos Javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+window.alert('Minha primeira mensagem'); // Alerta do servidor.
+
+window.confirm('Está gostando de Javascript?'); // Janela de confirmação ou negação.
+
+window.prompt('Qual é o seu nome?'); // Vai perguntar o nome...
