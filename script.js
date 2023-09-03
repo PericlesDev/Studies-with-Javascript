2f699871c50b96a6842834231f2218c8a57f2369
unknown <PericlesDosSantosRamos@hotmail.com>
2023-09-03 11:13:16 -0300

    Variáveis e Tipos Primitivos

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+/* Caixas de diálogo - Aula 1
+
 window.alert('Minha primeira mensagem'); // Alerta do servidor.
 
 window.confirm('Está gostando de Javascript?'); // Janela de confirmação ou negação.
 
 window.prompt('Qual é o seu nome?'); // Vai perguntar o nome...
+*/
+
+
+/* Variáveis e Tipos primitivos
+
+Variável  Atribuição  Valor
+
+var nome      =       "Péricles dos Santos Ramos"
+var idade     =       20
+var salário   =       1320.00
+var p1 = 15
+var p2 = 5 
+var p1 + p2 =
+*/
